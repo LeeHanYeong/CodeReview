c689bda934fbb57593bffa922e21fa80c32f999a
devand <devand@xtr>
2016-11-05 22:54:55 +0900

    목차 추가

--- django_app/codereview/urls.py
+++ django_app/codereview/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from djangokor.views import main
+from djangokor.views import main,  document
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^document/', document, name="document"),
     url(r'^$', main, name="main"),
+
+
 ]

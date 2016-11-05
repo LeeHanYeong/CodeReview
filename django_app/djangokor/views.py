b618f9611cff34a9593ff5f537ad8660d7d60007
devand <devand@xtr>
2016-11-05 23:51:13 +0900

    show me the money

--- django_app/djangokor/views.py
+++ django_app/djangokor/views.py
@@ -8,5 +8,5 @@ def main(request):
 
 def document(request):
     template_name = "djangokor/document.html"
-    print("document print")
+    print("document print show me the money ")
     return render(request, template_name, {'title': '목차','text':'장고 한글 문서 목차'})
